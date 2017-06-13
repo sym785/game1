3aa305b604acbeab45eca5f3b5eb822bde7f642a
sym785 <sym785@gmail.com>
2017-06-13 14:43:53 +0900

    fifth commit

---
+++ gameObjects/BaseObject.js
@@ -0,0 +1,7 @@
+function BaseObject() {
+  this.status = {};
+  this.status.x = 0;
+  this.status.y = 0;
+}
+
+module.exports = BaseObject;
---
+++ gameObjects/SETTINGS.js
@@ -0,0 +1,16 @@
+var SETTINGS = {
+  WIDTH : 700,
+  HEIGHT : 400,
+  BORDER_WIDTH : 15,
+  BACKGROUND_COLOR : "#FFFFFF",
+  PLAYER : {
+    WIDTH : 15,
+    HEIGHT : 80
+  },
+  BALL : {
+    WIDTH : 15,
+    HEIGHT : 15
+  }
+};
+
+module.exports = SETTINGS;
